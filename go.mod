be76165ca785da2fde8fbbd9216a6e9b76712059
Andrew Snodgrass <pointlander@gmail.com>
2024-03-19 16:19:24 -0500

    Added neural network

--- go.mod
+++ go.mod
@@ -2,4 +2,21 @@ module github.com/pointlander/srnn
 
 go 1.19
 
-require github.com/pointlander/datum/bible v0.0.0-20200802052503-0ee610caba95 // indirect
+require (
+	git.sr.ht/~sbinet/gg v0.5.0 // indirect
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
+	github.com/campoy/embedmd v1.0.0 // indirect
+	github.com/go-fonts/liberation v0.3.1 // indirect
+	github.com/go-latex/latex v0.0.0-20230307184459-12ec69307ad9 // indirect
+	github.com/go-pdf/fpdf v0.8.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/pointlander/datum/bible v0.0.0-20200802052503-0ee610caba95 // indirect
+	github.com/pointlander/gradient v0.0.0-20240226214843-e3d2a19564fd // indirect
+	github.com/ziutek/blas v0.0.0-20190227122918-da4ca23e90bb // indirect
+	golang.org/x/image v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	gonum.org/v1/plot v0.14.0 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -6,9 +6,16 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
+	"strings"
 
 	"github.com/pointlander/datum/bible"
+	"github.com/pointlander/gradient/tf64"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
 )
 
 const (
@@ -25,7 +32,7 @@ const (
 	// B2 exponential decay rate for the second-moment estimates
 	B2 = 0.89
 	// Eta is the learning rate
-	Eta = .0001
+	Eta = .01
 )
 
 const (
@@ -64,8 +71,8 @@ func Markov(verses []bible.Verse) *[256][256][256]float32 {
 }
 
 func main() {
-	rng := rand.New(rand.NewSource(1))
-	_ = rng
+	seed := int64(1)
+	rng := rand.New(rand.NewSource(seed))
 
 	bible, err := bible.Load()
 	if err != nil {
@@ -73,11 +80,55 @@ func main() {
 	}
 	verses := bible.GetVerses()
 	markov := Markov(verses)
-	buffer, index, n := make([]*[256]float32, 256), 0, 0
 	sum, sumSquared := make([]float32, Symbols), make([]float32, Symbols*Symbols)
 	mean, stddev := make([]float32, Symbols), make([]float32, Symbols*Symbols)
+
+	input := tf64.NewV(Symbols*Symbols+2*Symbols, 1)
+	input.X = input.X[:cap(input.X)]
+	output := tf64.NewV(Symbols, 1)
+	output.X = output.X[:cap(output.X)]
+
+	set := tf64.NewSet()
+	set.Add("w1", Symbols*Symbols+2*Symbols, 4*Symbols)
+	set.Add("b1", 4*Symbols)
+	set.Add("w2", 4*Symbols, Symbols)
+	set.Add("b2", Symbols)
+
+	l1 := tf64.Sigmoid(tf64.Add(tf64.Mul(set.Get("w1"), input.Meta()), set.Get("b1")))
+	loss := tf64.CrossEntropy(tf64.Softmax(tf64.Add(tf64.Mul(set.Get("w2"), l1), set.Get("b2"))), output.Meta())
+
+	for i := range set.Weights {
+		w := set.Weights[i]
+		size := w.S[0] * w.S[1]
+		if strings.HasPrefix(w.N, "b") {
+			w.X = w.X[:size]
+			w.States = make([][]float64, StateTotal)
+			for i := range w.States {
+				w.States[i] = make([]float64, len(w.X))
+			}
+			continue
+		}
+		factor := math.Sqrt(2.0 / float64(w.S[0]))
+		for i := 0; i < size; i++ {
+			w.X = append(w.X, rng.NormFloat64()*factor)
+		}
+		w.States = make([][]float64, StateTotal)
+		for i := range w.States {
+			w.States[i] = make([]float64, len(w.X))
+		}
+	}
+
+	points := make(plotter.XYs, 0, 8)
+	epoch := 0
+	total := 0.0
+	pow := func(x float64) float64 {
+		y := math.Pow(x, float64(epoch+1))
+		if math.IsNaN(y) || math.IsInf(y, 0) {
+			return 0
+		}
+		return y
+	}
 	for x, verse := range verses {
-		fmt.Println(x)
 		a, b := 0, 0
 		for y := range sum {
 			sum[y] = 0
@@ -85,7 +136,10 @@ func main() {
 		for y := range sumSquared {
 			sumSquared[y] = 0
 		}
-		for _, v := range verse.Verse {
+		buffer, index, n := make([]*[256]float32, 256), 0, 0
+		cost := 0.0
+		set.Zero()
+		for vv, v := range verse.Verse[:len(verse.Verse)-1] {
 			buffer[index] = &markov[a][b]
 			a, b = int(v), a
 			if n > 0 {
@@ -97,15 +151,131 @@ func main() {
 					}
 				}
 				n := float32(n)
+				s := float32(0)
 				for k, value := range sum {
 					mean[k] = value / n
+					s += mean[k]
 				}
+				t := 0
+				for _, value := range mean {
+					if s > 0 {
+						input.X[t] = float64(value / s)
+					} else {
+						input.X[t] = 0
+					}
+					t++
+				}
+				s = 0
 				for k, value := range sumSquared {
-					stddev[k] = (value / n) - mean[k/256]*mean[k%256]
+					stddev[k] = float32(math.Sqrt(float64((value / n) - mean[k/256]*mean[k%256])))
+					s += stddev[k]
 				}
+				for _, value := range stddev {
+					if s > 0 {
+						input.X[t] = float64(value / s)
+					} else {
+						input.X[t] = 0
+					}
+					t++
+				}
+				s = 0
+				for _, value := range buffer[index] {
+					s += value
+				}
+				for _, value := range buffer[index] {
+					if s > 0 {
+						input.X[t] = float64(value / s)
+					} else {
+						input.X[t] = 0
+					}
+					t++
+				}
+				for k := range output.X {
+					output.X[k] = 0
+				}
+				output.X[verse.Verse[vv+1]] = 1
+				cost += tf64.Gradient(loss).X[0]
 			}
+
 			index = (index + 1) % 256
 			n++
 		}
+		cost /= float64(len(verse.Verse))
+		total += cost
+		fmt.Println(x, cost)
+		points = append(points, plotter.XY{X: float64(x), Y: float64(cost)})
+
+		norm := 0.0
+		for _, p := range set.Weights {
+			for _, d := range p.D {
+				norm += d * d
+			}
+		}
+		norm = math.Sqrt(norm)
+		b1, b2 := pow(B1), pow(B2)
+		if norm > 1 {
+			scaling := 1 / norm
+			for _, w := range set.Weights {
+				if w.N == "w2" {
+					continue
+				}
+				for l, d := range w.D {
+					g := d * scaling
+					m := B1*w.States[StateM][l] + (1-B1)*g
+					v := B2*w.States[StateV][l] + (1-B2)*g*g
+					w.States[StateM][l] = m
+					w.States[StateV][l] = v
+					mhat := m / (1 - b1)
+					vhat := v / (1 - b2)
+					if vhat < 0 {
+						vhat = 0
+					}
+					w.X[l] -= Eta * mhat / (math.Sqrt(vhat) + 1e-8)
+				}
+			}
+		} else {
+			for _, w := range set.Weights {
+				if w.N == "w2" {
+					continue
+				}
+				for l, d := range w.D {
+					g := d
+					m := B1*w.States[StateM][l] + (1-B1)*g
+					v := B2*w.States[StateV][l] + (1-B2)*g*g
+					w.States[StateM][l] = m
+					w.States[StateV][l] = v
+					mhat := m / (1 - b1)
+					vhat := v / (1 - b2)
+					if vhat < 0 {
+						vhat = 0
+					}
+					w.X[l] -= Eta * mhat / (math.Sqrt(vhat) + 1e-8)
+				}
+			}
+		}
+	}
+
+	err = set.Save(fmt.Sprintf("weights_%d_%d.w", seed, epoch), total, epoch)
+	if err != nil {
+		panic(err)
+	}
+
+	p := plot.New()
+
+	p.Title.Text = "epochs vs cost"
+	p.X.Label.Text = "epochs"
+	p.Y.Label.Text = "cost"
+
+	scatter, err := plotter.NewScatter(points)
+	if err != nil {
+		panic(err)
+	}
+	scatter.GlyphStyle.Radius = vg.Length(1)
+	scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+	p.Add(scatter)
+
+	err = p.Save(8*vg.Inch, 8*vg.Inch, "epochs.png")
+	if err != nil {
+		panic(err)
 	}
 }
